459f240dbbd79de1b143c906559333a1ba9b1e48
Rishav-R03 <rishav042023@gmail.com>
2025-10-24 01:45:56 +0530

    Usermodel file update

--- src/main/java/com/rishav/urlshortener/models/UserModel.java
+++ src/main/java/com/rishav/urlshortener/models/UserModel.java
@@ -1,4 +1,47 @@
 package com.rishav.urlshortener.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(unique = true,nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
