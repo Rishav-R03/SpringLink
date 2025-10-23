248aa6fe5e978a0d6f5f5d1d6ac02ee78f320888
Rishav-R03 <rishav042023@gmail.com>
2025-10-24 01:46:31 +0530

    Userrepo file update

--- src/main/java/com/rishav/urlshortener/repository/UserRepo.java
+++ src/main/java/com/rishav/urlshortener/repository/UserRepo.java
@@ -1,4 +1,12 @@
 package com.rishav.urlshortener.repository;
 
-public interface UserRepo {
+import com.rishav.urlshortener.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<UserModel,Long> {
+    Optional<UserModel> findByUsername(String username);
 }
